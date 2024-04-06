5ade12a111da0f5653597af0eeb59a5665aedacc
Chris Mann <chrispmann@gmail.com>
2024-04-06 16:09:37 -0700

    unified jwa with jwa-st., functional

--- jwa-st.py
+++ jwa-st.py
@@ -9,33 +9,38 @@ from langchain_core.prompts.chat import (
 from langchain_openai import ChatOpenAI
 import streamlit as st
 
+load_dotenv()
+
 chat = ChatOpenAI(temperature=0)
 
-load_dotenv()
+input_text = st.text_area(label="Enter your prompt", height=20)
 
 messages = [
     SystemMessage(
         content="""
         
-        You are a Japanese teacher for native English speaking students. When presented with Japanese text, you give advice in English about how to improve it.
+        You are an advanced Japanese teacher for native English speaking students. 
+        When presented with Japanese text, you give advice in English about how to improve it.
+        You will not rewrite the whole thing yourself; that is an impedement to learning.
+        Think about grammar and vocabulary, but also consistency of style.
+        Make sure the writing is all in the same level of politeness.
+        There's no reason to translate anything that the user wrote to English; it's assumed that the user can read it just fine.
+        
         """
     ),
     HumanMessage(
-        content="私の名前がクリスです"
+        content=input_text
     ),
 ]
-# response = chat.invoke(messages)
+
 
 
 # Initialize session state for storing generated text if not already done
 if 'generated_text' not in st.session_state:
     st.session_state.generated_text = ""
 
-# Text area for input
-input_text = st.text_area(label="Enter your prompt", height=20)
 
-
-
-if st.button('Generate'):
-    st.session_state.generated_text += chat.invoke(messages)
+if st.button('Respond'):
+#    st.session_state.generated_text += chat.invoke(messages).content
+    st.session_state.generated_text = chat.invoke(messages).content
     st.write(st.session_state.generated_text)
